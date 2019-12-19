07c3db1c5c31f61c780634b87acb3b79207dd551
grzegorz.mika <gmika@interia.pl>
2019-12-19 10:49:02 +0100

    filter with datetime picker

--- crm/accounts/filters.py
+++ crm/accounts/filters.py
@@ -1,14 +1,27 @@
 import django_filters
 
+from django.forms import DateInput
+
 from django_filters import DateFilter
 
 from .models import *
 
 class OrderFilter(django_filters.FilterSet):
 
-    start_date = DateFilter(field_name="date_created", lookup_expr='gte')
-    end_date = DateFilter(field_name="date_created", lookup_expr='lte')
-
+    start_date = DateFilter(field_name="date_created", 
+                            lookup_expr='gte',
+                            label='Created from',
+                            widget = DateInput(attrs={
+                                        'class': 'datepicker', 
+                                        'type':'date'}
+                                    ))
+    end_date = DateFilter(field_name="date_created", 
+                            lookup_expr='lte', 
+                            label='Created to',
+                            widget = DateInput(attrs={
+                                        'class': 'datepicker',  
+                                        'type':'date'}
+                                    ))
     class Meta:
         model = Order
         fields = '__all__'
